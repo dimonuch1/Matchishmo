5434e1f3729e382e0337b2c36be673023ed11224
dimonuch1 <dimonuch1@gmail.com>
2016-12-02 05:59:10 -0800

    First commit

---
+++ Matchishmo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchishmo
+//
+//  Created by dima on 02/12/2016.
+//  Copyright © 2016 dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchishmo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchishmo
+//
+//  Created by dima on 02/12/2016.
+//  Copyright © 2016 dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
